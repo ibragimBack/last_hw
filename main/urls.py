760c689084123657cd7f5099784353efcbbd9756
ibragimBack <aibarchyn61@gmail.com>
2024-03-17 01:43:15 +0600

    вёрстка и оформление админки

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
+from django.conf.urls.static import (static)
 from django.conf import settings
 from book.views import post_library_view, post_librarys_detail_view
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', post_library_view),
     path('book/<int:id>/', post_librarys_detail_view),
+    path('^i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
